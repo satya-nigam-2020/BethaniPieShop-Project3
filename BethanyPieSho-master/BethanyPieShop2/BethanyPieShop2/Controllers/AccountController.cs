28ed6a06afe06416f1745448f1246fad80ada301
Satya Prakash Nigam <satya.nigam@zensar.com>
2020-08-30 20:22:27 +0530

    Almost complete!

---
+++ BethanyPieSho-master/BethanyPieShop2/BethanyPieShop2/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using BethanyPieShop2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BethanyPieShop2.Controllers
+{
+    public class AccountController : Controller
+    {
+       // DbContextClass _context;
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult YourOrder()
+        {
+            return View();
+        }
+
+        public ActionResult CartItems()
+        {
+            return View();
+        }
+
+        public ActionResult AccountDetails()
+        {
+            //var singleCustomer = _context.Addresses.SingleOrDefault(c => c.UserId == id);
+            //return View(singleCustomer);
+            return View();
+        }
+    }
+}
